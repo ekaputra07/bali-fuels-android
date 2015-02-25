b3f974721394fc4fcee653f734e8b3056e3b2cc5
Eka Putra <ekaputra@balitechy.com>
2015-02-26 00:28:41 +0800

    Testing parse geopoint range query

--- app/src/main/java/com/balitechy/balipetrols/MainFragment.java
+++ app/src/main/java/com/balitechy/balipetrols/MainFragment.java
@@ -1,5 +1,9 @@
 package com.balitechy.balipetrols;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -13,9 +17,20 @@ import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseGeoPoint;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
-public class MainFragment extends Fragment implements OnMapReadyCallback {
+import java.util.List;
+
+public class MainFragment extends Fragment implements OnMapReadyCallback{
+
     private MapView mapView;
+    private int locationChangedCounter = 0;
+    private LocationManager locationManager;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -26,15 +41,6 @@ public class MainFragment extends Fragment implements OnMapReadyCallback {
     }
 
     @Override
-    public void onMapReady(GoogleMap map) {
-        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        map.setMyLocationEnabled(true);
-
-        LatLng centerPos = new LatLng(-8.3809918, 115.1761822); //Bali
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(centerPos, 10));
-    }
-
-    @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
@@ -42,6 +48,60 @@ public class MainFragment extends Fragment implements OnMapReadyCallback {
         mapView.onCreate(savedInstanceState);
         mapView.onResume();
         MapsInitializer.initialize(getActivity().getApplicationContext());
+    }
+
+    @Override
+    public void onMapReady(final GoogleMap map) {
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        map.setMyLocationEnabled(true);
+
+        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                // on first location update, move camera to that position.
+                if(locationChangedCounter == 0) {
+                    LatLng centerPos = new LatLng(location.getLatitude(), location.getLongitude());
+                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(centerPos, 13));
+                }
+                locationChangedCounter++;
+
+                findPetrolsNearby(map, location);
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        });
+    }
+
+    private void findPetrolsNearby(final GoogleMap map, Location location){
+        ParseGeoPoint currentPoint = new ParseGeoPoint(location.getLatitude(), location.getLongitude());
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("GasStation");
+        query.whereWithinKilometers("point", currentPoint, 5); //1KM
+
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> locations, ParseException e) {
+                if (e == null) {
+                    for (ParseObject loc : locations) {
+                        LatLng point = new LatLng(loc.getParseGeoPoint("point").getLatitude(), loc.getParseGeoPoint("point").getLongitude());
+                        map.addMarker(new MarkerOptions().position(point));
+                    }
+                }
+            }
+        });
     }
 
     @Override
