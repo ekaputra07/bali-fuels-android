2db8c16b7089761635413d381869b0c3675564be
Eka Putra <ekaputra@balitechy.com>
2015-02-27 00:33:06 +0800

    Testing search range and radius seeker

--- app/src/main/java/com/balitechy/balipetrols/MainFragment.java
+++ app/src/main/java/com/balitechy/balipetrols/MainFragment.java
@@ -1,6 +1,7 @@
 package com.balitechy.balipetrols;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -10,13 +11,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.parse.FindCallback;
 import com.parse.ParseException;
@@ -24,18 +29,31 @@ import com.parse.ParseGeoPoint;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class MainFragment extends Fragment implements OnMapReadyCallback{
+public class MainFragment extends Fragment implements OnMapReadyCallback, LocationListener, SeekBar.OnSeekBarChangeListener{
 
     private MapView mapView;
+    private SeekBar radiusSeeker;
+    private List<Marker> markers = new ArrayList<Marker>();
+
     private int locationChangedCounter = 0;
+    private Location lastLocation;
     private LocationManager locationManager;
+    private GoogleMap map;
+    private Circle radiusCircle;
+    private final int maxRadius = 10; // 50KM
+    private final int defaultRadius = 2; // 5KM
+    private double currentRadius = (double) defaultRadius;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         mapView = (MapView) rootView.findViewById(R.id.map);
+        radiusSeeker = (SeekBar) rootView.findViewById(R.id.radiusSeeker);
+        radiusSeeker.setProgress((int) ((defaultRadius * 100.0f) / maxRadius));
+        radiusSeeker.setOnSeekBarChangeListener(this);
 
         return rootView;
     }
@@ -51,74 +69,126 @@ public class MainFragment extends Fragment implements OnMapReadyCallback{
     }
 
     @Override
-    public void onMapReady(final GoogleMap map) {
+    public void onMapReady(GoogleMap map) {
         map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         map.setMyLocationEnabled(true);
+        map.getUiSettings().setMyLocationButtonEnabled(true);
+        this.map = map;
 
         locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-                // on first location update, move camera to that position.
-                if(locationChangedCounter == 0) {
-                    LatLng centerPos = new LatLng(location.getLatitude(), location.getLongitude());
-                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(centerPos, 13));
-                }
-                locationChangedCounter++;
-
-                findPetrolsNearby(map, location);
-            }
-
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String provider) {
-
-            }
-
-            @Override
-            public void onProviderDisabled(String provider) {
-
-            }
-        });
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, this);
     }
 
-    private void findPetrolsNearby(final GoogleMap map, Location location){
-        ParseGeoPoint currentPoint = new ParseGeoPoint(location.getLatitude(), location.getLongitude());
+    @Override
+    public void onResume() {
+        mapView.onResume();
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        mapView.onPause();
+        if(locationManager != null){
+            locationManager.removeUpdates(this);
+        }
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        mapView.onDestroy();
+        if(locationManager != null){
+            locationManager.removeUpdates(this);
+        }
+        super.onDestroy();
+    }
+
+    /* ---------------- OnLocationChange -------------------------*/
+
+    @Override
+    public void onLocationChanged(Location location) {
+        lastLocation = location;
+
+        // on first location update, move camera to that position.
+        if(locationChangedCounter == 0) {
+            LatLng centerPos = new LatLng(location.getLatitude(), location.getLongitude());
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(centerPos, 13));
+        }
+        locationChangedCounter++;
+
+        LatLng newPos = new LatLng(location.getLatitude(), location.getLongitude());
+        if(radiusCircle == null){
+            int baseColor = Color.BLUE;
+            radiusCircle = map.addCircle(
+                    new CircleOptions()
+                            .center(newPos)
+                            .radius(defaultRadius * 1000) //5 x 1000 in meters.
+                            .strokeColor(Color.BLUE).strokeWidth(1)
+                            .fillColor(Color.argb(10, Color.red(baseColor), Color.green(baseColor), Color.blue(baseColor)))
+            );
+        }else{
+            radiusCircle.setCenter(newPos);
+        }
+
+        findPetrolsNearby();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+    @Override
+    public void onProviderEnabled(String provider) {}
+
+    @Override
+    public void onProviderDisabled(String provider) {}
+
+
+    private void findPetrolsNearby(){
+        ParseGeoPoint currentPoint = new ParseGeoPoint(lastLocation.getLatitude(), lastLocation.getLongitude());
+
         ParseQuery<ParseObject> query = ParseQuery.getQuery("GasStation");
-        query.whereWithinKilometers("point", currentPoint, 5); //1KM
+        query.whereWithinKilometers("point", currentPoint, currentRadius);
 
         query.findInBackground(new FindCallback<ParseObject>() {
             @Override
             public void done(List<ParseObject> locations, ParseException e) {
                 if (e == null) {
+
+                    //Clean markers
+                    for (Marker m: markers){
+                        m.remove();
+                    }
+                    markers.clear();
+
+                    // add new markers
                     for (ParseObject loc : locations) {
                         LatLng point = new LatLng(loc.getParseGeoPoint("point").getLatitude(), loc.getParseGeoPoint("point").getLongitude());
-                        map.addMarker(new MarkerOptions().position(point));
+                        Marker marker = map.addMarker(new MarkerOptions().position(point));
+                        markers.add(marker);
                     }
                 }
             }
         });
     }
 
+    /* ---------------- OnSeekBarChange -------------------*/
+
     @Override
-    public void onResume() {
-        super.onResume();
-        mapView.onResume();
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        if(radiusCircle != null){
+            double newRadiusScale = (double) ((progress * maxRadius) / 100.0f);
+            radiusCircle.setRadius(newRadiusScale * 1000);
+            currentRadius = newRadiusScale;
+        }
     }
 
     @Override
-    public void onPause() {
-        super.onPause();
-        mapView.onPause();
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
     }
 
     @Override
-    public void onDestroy() {
-        super.onDestroy();
-        mapView.onDestroy();
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        findPetrolsNearby();
     }
 }
